e4d5a4e89ca4659f14a37c392bad1b7d2ec6953d
Michael Bardsley <michael.bardsley@monash.edu>
2020-03-04 21:03:20 +1100

    remove cruft

--- examples/thermal_potential_2d/thermal_potential.cpp
+++ examples/thermal_potential_2d/thermal_potential.cpp
@@ -26,55 +26,6 @@ struct Thermal_Vacua {
     double val_false_vacuum;
 };
 
-// Thermal_Vacua locate_vacua(Simple2DPotential potential,
-//     Eigen::VectorXd hint_false, Eigen::VectorXd hint_true, 
-//     double bbox_x, double bbox_y) {
-    
-//     Thermal_Vacua vacua;
-
-//     const auto v = [&potential](const Eigen::VectorXd& x) -> double {
-//         return potential(x);
-//     };
-
-//     Eigen::VectorXd lower_bounds(2);
-//     Eigen::VectorXd upper_bounds(2);
-
-//     NLopt_optimizer optimizer(v, 2);
-//     optimizer.set_extremum_type(NLopt_optimizer::Extremum_type::MIN);
-//     optimizer.set_max_time(1.0);
-
-//     // Locate false vacuum
-//     lower_bounds << hint_false(0) - bbox_x, hint_false(1) - bbox_y;
-//     upper_bounds << hint_false(0) + bbox_x, hint_false(1) + bbox_y;
-//     optimizer.set_lower_bounds(lower_bounds);
-//     optimizer.set_upper_bounds(upper_bounds);
-
-//     auto status = optimizer.optimize(hint_false);
-//     if (!optimization_succeeded(status)) {
-//         std::cerr << "Error: unable to locate false vacuum." << std::endl;
-//         exit(EXIT_FAILURE);
-//     }
-
-//     vacua.false_vacuum = optimizer.get_extremum_location();
-//     vacua.val_false_vacuum = optimizer.get_extremum_value();
-
-//     // Locate true vacuum
-//     lower_bounds << hint_true(0) - bbox_x, hint_true(1) - bbox_y;
-//     upper_bounds << hint_true(0) + bbox_x, hint_true(1) + bbox_y;
-//     optimizer.set_lower_bounds(lower_bounds);
-//     optimizer.set_upper_bounds(upper_bounds);
-
-//     status = optimizer.optimize(hint_true);
-//     if (!optimization_succeeded(status)) {
-//         std::cerr << "Error: unable to locate true vacuum." << std::endl;
-//         exit(EXIT_FAILURE);
-//     }
-
-//     vacua.true_vacuum = optimizer.get_extremum_location();
-//     vacua.val_true_vacuum = optimizer.get_extremum_value();
-//     return vacua;
-// }
-
 double find_minimum(std::vector<data_row> grid) {
     double min = std::numeric_limits<double>::infinity();
 
@@ -129,7 +80,6 @@ void plot_grid(std::vector<data_row> grid, std::string title) {
     gp << "set xlabel 'x'\n";
     gp << "set ylabel 'y'\n";
     
-    // gp << "plot '-' u 1:2:3 w image not, '/tmp/contour.txt' u 1:2 lc black w l not\n";
     gp << "plot '-' u 1:2:3 w image not, '/tmp/contour.txt' u 1:2 w l not\n";
 
     gp.send1d(grid);
@@ -143,10 +93,7 @@ void temperature_plot(Simple2DPotential potential) {
     double x_max = plot_scale;
     double y_min = -plot_scale;
     double y_max = plot_scale;
-    // double x_min = -1;
-    // double x_max = 1;
-    // double y_min = -1;
-    // double y_max = 1;
+
     int axis_size = 200;
 
     std::ostringstream title;
